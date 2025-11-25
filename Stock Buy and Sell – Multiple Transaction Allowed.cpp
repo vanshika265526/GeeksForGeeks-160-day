68f571834049030d335e21845ea3832cb3cb9def
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2025-11-25 18:57:40 +0530

    Create Stock Buy and Sell – Multiple Transaction Allowed.cpp

---
+++ Stock Buy and Sell – Multiple Transaction Allowed.cpp
@@ -0,0 +1,26 @@
+/*
+Stock Buy and Sell – Multiple Transaction Allowed
+Difficulty: MediumAccuracy: 13.43%Submissions: 191K+Points: 4
+The cost of stock on each day is given in an array price[]. Each day you may decide to either buy or sell the stock i at price[i], you can even buy and sell the stock on the same day. Find the maximum profit that you can get.
+
+Note: A stock can only be sold if it has been bought previously and multiple stocks cannot be held on any given day.
+
+*/
+
+
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        // basically we will add profits whose difference is greater than previous 
+        int profit=0;  //initialise the profit variable first
+        
+        //start the loop so that we can check for differences
+        for(int i=0;i<prices.size()-1;i++){
+            if(prices[i]<prices[i+1]){
+                profit+=prices[i+1]-prices[i];      
+            }
+        }    
+        return profit;
+    }
+};
+
