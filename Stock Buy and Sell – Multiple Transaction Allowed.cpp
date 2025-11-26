c056233617499d7eab0b4440fa91d3ae6a5b93f8
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2025-11-26 10:44:34 +0530

    Update Stock Buy and Sell – Multiple Transaction Allowed.cpp

--- Stock Buy and Sell – Multiple Transaction Allowed.cpp
+++ Stock Buy and Sell – Multiple Transaction Allowed.cpp
@@ -1,7 +1,8 @@
 /*
 Stock Buy and Sell – Multiple Transaction Allowed
-Difficulty: MediumAccuracy: 13.43%Submissions: 191K+Points: 4
-The cost of stock on each day is given in an array price[]. Each day you may decide to either buy or sell the stock i at price[i], you can even buy and sell the stock on the same day. Find the maximum profit that you can get.
+
+The cost of stock on each day is given in an array price[]. Each day you may decide to either buy or sell the stock i
+at price[i], you can even buy and sell the stock on the same day. Find the maximum profit that you can get.
 
 Note: A stock can only be sold if it has been bought previously and multiple stocks cannot be held on any given day.
 
