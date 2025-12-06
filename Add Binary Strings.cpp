0fa4d814febd45655b48d429ce57fce649338521
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2025-12-06 17:29:31 +0530

    Create Add Binary Strings.cpp

---
+++ Add Binary Strings.cpp
@@ -0,0 +1,11 @@
+/*
+Given two binary strings s1 and s2 consisting of only 0s and 1s. Find the resultant string after adding the two Binary Strings.
+Note: The input strings may contain leading zeros but the output string should not have any leading zeros.
+
+Input: s1 = "1101", s2 = "111"
+Output: 10100
+Explanation:
+ 1101
++ 111
+10100
+  */
