429b1dbc1a2ac61fb78f33caf51ed691100c3654
Vanshika <166399561+vanshika265526@users.noreply.github.com>
2025-12-14 19:24:20 +0530

    Create Count Inversions.cpp

---
+++ Sorting/Count Inversions.cpp
@@ -0,0 +1,10 @@
+/*
+Given an array of integers arr[]. You have to find the Inversion Count of the array. 
+Note : Inversion count is the number of pairs of elements (i, j) such that i < j and arr[i] > arr[j].
+
+Examples:
+
+Input: arr[] = [2, 4, 1, 3, 5]
+Output: 3
+Explanation: The sequence 2, 4, 1, 3, 5 has three inversions (2, 1), (4, 1), (4, 3).
+  */
